ca953c4428696b9deebcff58a182af533e73502b
Ryo Nakae <ryo.n.1129@gmail.com>
2015-12-15 18:05:38 +0900

    Bower使うのやめてみる

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,6 +1,17 @@
 'use strict';
 
-require('jquery');
-require('velocity');
+(function(){
 
-console.log('Hello world!');
+
+window.jQuery = window.$ = require('jquery');
+var imagesLoaded = require('imagesloaded');
+var velocity = require('velocity-animate');
+
+imagesLoaded.makeJQueryPlugin($);
+$('.gallery').imagesLoaded(function(){
+  console.log('gallery loaded');
+  $("body").velocity({ opacity: 0 }, { duration: 1000 });
+});
+
+
+})();
