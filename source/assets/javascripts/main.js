7fb935013ae04c8907fa62d7a55ae1ee5e85cfc8
Ryo Nakae <ryo.n.1129@gmail.com>
2015-12-15 18:51:58 +0900

    アイキャッチの回転を雑に実装

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -3,15 +3,68 @@
 (function(){
 
 
+// require
 window.jQuery = window.$ = require('jquery');
-var imagesLoaded = require('imagesloaded');
 var velocity = require('velocity-animate');
-
+var imagesLoaded = require('imagesloaded');
 imagesLoaded.makeJQueryPlugin($);
-$('.gallery').imagesLoaded(function(){
-  console.log('gallery loaded');
-  $("body").velocity({ opacity: 0 }, { duration: 1000 });
+
+
+$('.eyecatch__model').imagesLoaded(function(){
+  console.log('model loaded');
+
+  $('.eyecatch__model').velocity({
+    opacity: 1
+  }, {
+    duration: 1200,
+    easing: 'ease',
+    complete: rotateModel
+  });
 });
+
+
+function rotateModel(){
+  var $stage = $('.eyecatch');
+  var $sprite = $('.eyecatch__modelList');
+  var spriteSize = $sprite.children().eq(0).height();
+  var spriteNumber = 30;
+  var spritePage = 0;
+  var pageX = 0;
+  // var thinMove = $stage.width() / spriteNumber * 0.5;
+  var thinMove = 10;
+
+  $stage.on('mouseenter', function(e){
+    pageX = e.pageX;
+  });
+
+  $stage.on('mousemove', function(e){
+    console.log('spritePage: ', spritePage);
+
+    if(e.pageX >= pageX + thinMove){
+      pageX = e.pageX;
+
+      if(spritePage < spriteNumber){
+        spritePage++;
+      }else{
+        spritePage = 0;
+      };
+    }else if(e.pageX <= pageX - thinMove){
+      pageX = e.pageX;
+
+      if(spritePage >= 0){
+        spritePage--;
+      }else{
+        spritePage = spriteNumber;
+      };
+    };
+
+    $sprite.velocity({
+      translateY: '-' + spriteSize * spritePage +  'px 0'
+    }, {
+      duration: 0
+    });
+  });
+};
 
 
 })();
